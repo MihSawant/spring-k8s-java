0e1bd4ec66a139a952fae3e1012221972e4d509c
Mihir Sawant <mihirssawant06@gmail.com>
2023-01-01 05:15:34 +0530

    initial commit

---
+++ src/main/java/sawant/mihir/springk8sready/SpringK8sReadyApplication.java
@@ -0,0 +1,13 @@
+package sawant.mihir.springk8sready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringK8sReadyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringK8sReadyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sawant/mihir/springk8sready/SpringK8sReadyApplicationTests.java
@@ -0,0 +1,13 @@
+package sawant.mihir.springk8sready;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringK8sReadyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
